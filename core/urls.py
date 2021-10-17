7d815f13298bc8df32c04320b8c498a894eb2396
Pulkit Kumar Agarwal <63954083+pka5667@users.noreply.github.com>
2021-10-17 20:08:41 +0530

    Product Details scrapper added

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('video', views.video, name="video"),
     path('contact', views.contact, name="contact"),
+    path('api/get_product_details', views.get_product_details, name="get_product_details"),
 ]
